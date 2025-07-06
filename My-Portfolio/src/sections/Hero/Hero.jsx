a7ebd03d46ea33e1e64a6567deb86c89d56116ec
Anshul Bhatt <anb201982@gmail.com>
2025-07-06 21:21:41 +0530

    updated Hero.jsx

--- My-Portfolio/src/sections/Hero/Hero.jsx
+++ My-Portfolio/src/sections/Hero/Hero.jsx
@@ -5,10 +5,21 @@ import themeIcon from '../../assets/sun.svg';
 
 const Hero = () => {
   return (
-    <section id='hero'>
+    <section id='hero' className='container'>
       <div className='colorModeContainer'>
         <img className='hero' src={heroImg} alt="Hero" />
         <img className='comorMode' src={themeIcon} alt="mode icon" />
+      </div>
+      <div className="info">
+        <h1>Anshul
+            <br />
+            Bhatt
+        </h1>
+        <h2>Frontend Developer</h2>
+        <span>
+            
+        </span>
+
       </div>
     </section>
   );
