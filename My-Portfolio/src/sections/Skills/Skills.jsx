cd0405cf5314c96ad7f3bb39644ae8135fa1d785
Anshul Bhatt <anb201982@gmail.com>
2025-07-31 23:03:21 +0530

    updated

--- My-Portfolio/src/sections/Skills/Skills.jsx
+++ My-Portfolio/src/sections/Skills/Skills.jsx
@@ -8,20 +8,31 @@ const Skills = () => {
     <section id="skills" className="skillsContainer">
       <h1>Skills</h1>
       <div className="skillList">
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="NodeJs" />
-        
-        <SkillList src={checkMarkIcon} skill="ReactJs" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
-        <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="GitHub" />
-        
-        <SkillList src={checkMarkIcon} skill="C" />
-        <SkillList src={checkMarkIcon} skill="C++" />
-        <SkillList src={checkMarkIcon} skill="Java" />
-      </div>
+  <div className="skillGroup">
+    <SkillList src={checkMarkIcon} skill="HTML" />
+    <SkillList src={checkMarkIcon} skill="CSS" />
+    <SkillList src={checkMarkIcon} skill="JavaScript" />
+    <SkillList src={checkMarkIcon} skill="NodeJs" />
+  </div>
+
+  <hr />
+
+  <div className="skillGroup">
+    <SkillList src={checkMarkIcon} skill="ReactJs" />
+    <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+    <SkillList src={checkMarkIcon} skill="Git" />
+    <SkillList src={checkMarkIcon} skill="GitHub" />
+  </div>
+
+  <hr />
+
+  <div className="skillGroup">
+    <SkillList src={checkMarkIcon} skill="C" />
+    <SkillList src={checkMarkIcon} skill="C++" />
+    <SkillList src={checkMarkIcon} skill="Java" />
+  </div>
+</div>
+
     </section>
   );
 };
